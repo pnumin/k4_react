4eca6a5f0290fae35582f1c8b76f5827dfca7445
pnumin <pnumin@pusan.ac.kr>
2023-10-05 13:01:22 +0900

    useRefBox

--- src/04/Box.js
+++ src/04/Box.js
@@ -1,75 +1,58 @@
-import { useState, useEffect } from "react";
+import Hh1 from '../comm/Hh1' ;
 import style from './Box.module.css' ;
+import { useRef, useState, useEffect } from 'react';
 
 const Box = () => {
-    const [boxlist, setBoxlist] = useState();
-    const [listTag, setListTag] = useState();
-    const [detailTag, setDetailTag] = useState() ;
+    //날짜선택 
+    const dt = useRef() ;
 
-    const handleClick = (item) => {
-        console.log(item)
-        setDetailTag(
-            <div className={style.numDiv}>
-                <span>[{item.movieCd}]</span>
-                <span>개봉일 : {item.openDt} {item.rankOldAndNew}</span>
-                <span>{item.movieNm}</span>
-            </div>
-        );
-    }
+    //선택된 날짜
+    const [cdt, setCdt] = useState() ;
 
-    //컴포넌트 생성시 한번 실행
+    //컴포넌트 생성시 포커스 
     useEffect(() => {
-        let url = 'https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20230918';
-        fetch(url)
-            .then(resp => resp.json())
-            .then(data => setBoxlist(data.boxOfficeResult.dailyBoxOfficeList))
-            .catch(err => console.log(err));
+        const yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1); 
+
+        let y = `${yesterday.getFullYear()}` ;
+        let m = yesterday.getMonth()+1 < 10 ? `0${yesterday.getMonth()+1}`
+                                            :`${yesterday.getMonth()+1}`;
+                                            
+        let d = yesterday.getDate() < 10 ? `0${yesterday.getDate()}`
+                                            :`${yesterday.getDate()}`;
+        console.log(y+m+d) ;
+
+        //어제 날짜로 기본 값 설정
+        dt.current.value = `${y}-${m}-${d}` ;
+        setCdt(y+m+d) ;
     }, []);
 
-    //state 변수가 변경될때 마다 실행
-    useEffect(() => {
-        console.log(boxlist) ;
-        if (boxlist) {
-            setListTag(boxlist.map((item, idx) => 
-                <tr key={'mv'+idx} onClick={() => handleClick(item)}>
-                    <td>{item.rank}</td>
-                    <td>{item.movieNm}</td>
-                    <td>{parseInt(item.salesAmt).toLocaleString('ko-KR')}</td>
-                    <td>{
-                        item.rankInten == 0 
-                        ? "-" 
-                        : item.rankInten > 0 
-                            ? "▲"+ Math.abs(item.rankInten) : "▼"+Math.abs(item.rankInten)
-                        }</td>
-                </tr> 
-            ));
-        }
-    }, [boxlist]) ;
- 
+    useEffect(() =>{
+        console.log(cdt)
+    } , [cdt]);
+
+    //날짜가 변경되었을때 
+    const handleChange = () => {
+        let temp = dt.current.value.replaceAll('-','') ; 
+
+        setCdt(temp) ; 
+    }
+
     return (
-        <main className="container">
+        <main className="container"> 
+            <Hh1 title='박스오피스' /> 
             <article>
-                <header><h1>일일박스 오피스</h1></header>
-                <table>
-                    <thead>
-                        <tr>
-                        <th scope="col">순위</th>
-                        <th scope="col">영화명</th>
-                        <th scope="col">매출액</th>
-                        <th scope="col">증감</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {listTag}
-                    </tbody>
-                </table>
-                <footer>
-                    {detailTag}               
-                </footer>
+                <header>
+                    <div className={style.dt}>선택날짜 : {cdt}</div>
+                    <form>
+                        <input ref={dt} type='date' id='dt' name='dt' 
+                                onChange={handleChange}/>
+                    </form>
+                </header>
             </article>
-            
         </main>
-    );
+    )
+
 }
 
 export default Box;
--- src/07/MyRef.js
+++ src/07/MyRef.js
@@ -1,19 +1,72 @@
 import Hh1 from '../comm/Hh1';
+import { useState, useEffect, useRef } from 'react';
 
 const MyRef = () => {
-    const title='useRef Hook' ;
+    const title='useRef Hook : 변수제어' ;
+    //let cnt1 = 0 ;
+    const [cnt1, setCnt1] = useState() ;
+
+    //Ref변수
+    const cnt2 = useRef(0);
+    const txt1 = useRef() ;
+
+    const handleClick = () => {
+        //cnt1 = cnt1 + 1 ;
+        setCnt1(cnt1 + 1) ;
+        console.log("handleClick" , cnt1)
+    }
+
+    const handleClickRef = () => {
+        cnt2.current = cnt2.current + 1 ;
+        console.log("handleClickRef", cnt2.current)
+    }
+
+    const handleChange = () => {
+        console.log("handleChange", txt1.current.value)
+        setCnt1(parseInt(txt1.current.value));
+    }
+
+    //컴포넌트 생성시 1번 호출
+    useEffect(()=>{
+        setCnt1(100) ;
+
+        txt1.current.focus();
+    }, []);
+
+    //컴포넌트 업데이트시 생성 : cnt1 state변수가 업데이트될때
+    useEffect(() => {
+        console.log("useEffect", cnt1)
+    }, [cnt1]);
+
     return (
         <main className='container'>
             <article>
+                <Hh1 title='useRef Hook : Form 제어' /> 
+                <form>
+                    <input 
+                    ref={txt1} 
+                    type='number' 
+                    id='txt1' 
+                    name='txt1' 
+                    placeholder='숫자입력' 
+                    onChange={handleChange} />
+                </form>
+            </article>
+
+            <article>
                 <Hh1 title={title} /> 
-                
+                <div className='grid'>
+                    <div>state변수 : {cnt1}</div>
+                    <div>Ref변수 : {cnt2.current}</div>
+                </div>
                 <footer>
                     <div className='grid'>
-                        <button>state 변수 증가</button>
+                        <button onClick={handleClick}>state 변수 증가</button>
+                        <button onClick={handleClickRef}>Ref 변수 증가</button>
                     </div>
-                </footer>
-                
+                </footer>               
             </article>
+            
             
         </main>
     )
